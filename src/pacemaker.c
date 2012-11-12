dce37852451b0af4df46de279af1885c66d5a703
Yuichi SEINO <seino.cluster2@gmail.com>
2012-11-12 11:48:28 +0900

    LOG:Add strace to crm_ticket Signed-off-by: Yuichi SEINO <seino.cluster2@gmail.com>

--- src/pacemaker.c
+++ src/pacemaker.c
@@ -22,12 +22,24 @@
 
 #define COMMAND_MAX	256
 
+#define GRANT_ST "strace -o /tmp/g_ticket.strace"
+#define REVOKE_ST "strace -o /tmp/r_ticket.strace"
+
+#define S_OWN_ST "strace -o /tmp/s_own.strace"
+#define S_EXP_ST "strace -o /tmp/s_exp.strace"
+#define S_BAL_ST "strace -o /tmp/s_bal.strace"
+
+#define L_OWN_ST "strace -o /tmp/l_own.strace"
+#define L_EXP_ST "strace -o /tmp/l_exp.strace"
+#define L_BAL_ST "strace -o /tmp/l_bal.strace"
+
 static void pcmk_grant_ticket(const void *ticket)
 {
 	FILE *p;
 	char cmd[COMMAND_MAX];
 
-	snprintf(cmd, COMMAND_MAX, "crm_ticket -t %s -g --force",
+	snprintf(cmd, COMMAND_MAX, "%s crm_ticket -t %s -g --force",
+			GRANT_ST,
 		 (char *)ticket);
 	log_info("command: '%s' was executed", cmd);
 	p = popen(cmd, "r");
@@ -45,7 +57,8 @@ static void pcmk_revoke_ticket(const void *ticket)
 	FILE *p;
 	char cmd[COMMAND_MAX];
 
-	snprintf(cmd, COMMAND_MAX, "crm_ticket -t %s -r --force",
+	snprintf(cmd, COMMAND_MAX, "%s crm_ticket -t %s -r --force",
+			REVOKE_ST,
 		 (char *)ticket);
 	log_info("command: '%s' was executed", cmd);
 	p = popen(cmd, "r");
@@ -65,7 +78,8 @@ static void pcmk_store_ticket(const void *ticket, int owner, int ballot,
 	char cmd[COMMAND_MAX];
 
 	snprintf(cmd, COMMAND_MAX,
-		 "crm_ticket -t %s -S owner -v %d",
+		 "%s crm_ticket -t %s -S owner -v %d",
+		 S_OWN_ST,
 		 (char *)ticket, owner);
 	log_info("command: '%s' was executed", cmd);
 	p = popen(cmd, "r");
@@ -76,7 +90,8 @@ static void pcmk_store_ticket(const void *ticket, int owner, int ballot,
 	pclose(p);
 
 	snprintf(cmd, COMMAND_MAX,
-		 "crm_ticket -t %s -S expires -v %llu",
+		 "%s crm_ticket -t %s -S expires -v %llu",
+		 S_EXP_ST,
 		 (char *)ticket, expires);
 	log_info("command: '%s' was executed", cmd);
 	p = popen(cmd, "r");
@@ -87,7 +102,8 @@ static void pcmk_store_ticket(const void *ticket, int owner, int ballot,
 	pclose(p);
 
 	snprintf(cmd, COMMAND_MAX,
-		 "crm_ticket -t %s -S ballot -v %d",
+		 "%s crm_ticket -t %s -S ballot -v %d",
+		 S_BAL_ST,
 		 (char *)ticket, ballot);
 	log_info("command: '%s' was executed", cmd);
 	p = popen(cmd, "r");
@@ -110,7 +126,8 @@ static void pcmk_load_ticket(const void *ticket, int *owner, int *ballot,
 	unsigned long long ex;
 
 	snprintf(cmd, COMMAND_MAX,
-		 "crm_ticket -t %s -G owner --quiet",
+		 "%s crm_ticket -t %s -G owner --quiet",
+		 L_OWN_ST,
 		 (char *)ticket);
 	log_info("command: '%s' was executed", cmd);
 	p = popen(cmd, "r");
@@ -127,7 +144,8 @@ static void pcmk_load_ticket(const void *ticket, int *owner, int *ballot,
 	pclose(p);
 	
 	snprintf(cmd, COMMAND_MAX,
-		 "crm_ticket -t %s -G expires --quiet",
+		 "%s crm_ticket -t %s -G expires --quiet",
+		 L_EXP_ST,
 		 (char *)ticket);
 	log_info("command: '%s' was executed", cmd);
 	p = popen(cmd, "r");
@@ -144,7 +162,8 @@ static void pcmk_load_ticket(const void *ticket, int *owner, int *ballot,
 	pclose(p);
 
 	snprintf(cmd, COMMAND_MAX,
-		 "crm_ticket -t %s -G ballot --quiet",
+		 "%s crm_ticket -t %s -G ballot --quiet",
+		 L_BAL_ST,
 		 (char *)ticket);
 	log_info("command: '%s' was executed", cmd);
 	p = popen(cmd, "r");
